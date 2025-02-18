ac5a5e98ad2e5760d5c71ca61ca5a8a3ae095ad6
Nick Saveliev <kolya59264@gmail.com>
2025-02-18 15:37:03 +0200

    Refactor validation part.

--- srcs/validation.c
+++ srcs/validation.c
@@ -25,11 +25,28 @@ static void	check_is_dir(char *arg, t_mshell *obj)
 	}
 }
 
-static char	*check_paths(char **paths, char **args, size_t *args_move)
+static char	*create_path(char *arg, size_t args_move, char *envp_path)
+{
+	size_t	path_len;
+	char	*path;
+
+	path_len = ft_strlen(envp_path) + ft_strlen(arg + args_move) + 2;
+	path = ft_calloc(path_len, sizeof(char));
+	if (!path)
+	{
+		ft_putstr_fd("Malloc failed\n", STDERR_FILENO);
+		return (NULL);
+	}
+	ft_strlcpy(path, envp_path, path_len);
+	ft_strlcat(path, "/", path_len);
+	ft_strlcat(path, arg + args_move, path_len);
+	return (path);
+}
+
+static char	*search_paths(char **paths, char **args, size_t *args_move)
 {
 	int		i;
 	char	*path;
-	size_t	path_len;
 
 	if (!args || !*args || !**args)
 		return (ft_strdup(""));
@@ -40,16 +57,7 @@ static char	*check_paths(char **paths, char **args, size_t *args_move)
 	i = 0;
 	while (paths[i])
 	{
-		path_len = ft_strlen(paths[i]) + ft_strlen((*args) + *args_move) + 2;
-		path = ft_calloc(path_len, sizeof(char));
-		if (!path)
-		{
-			ft_putstr_fd("Malloc failed\n", STDERR_FILENO);
-			return (NULL);
-		}
-		ft_strlcpy(path, paths[i], path_len);
-		ft_strlcat(path, "/", path_len);
-		ft_strlcat(path, (*args) + *args_move, path_len);
+		path = create_path(*args, *args_move, paths[i]);
 		if (access(path, F_OK) == 0)
 			return (path);
 		check_free_str(&path);
@@ -58,33 +66,17 @@ static char	*check_paths(char **paths, char **args, size_t *args_move)
 	return (NULL);
 }
 
-static char	**check_args(char **args)
+static char	*check_path(char *path, char *node_arg, t_mshell *obj, t_ast_node *node)
 {
-	static char	*new_args[] = {"", NULL};
-
-	if (!args)
-		return (new_args);
-	return (args);
-}
-
-char	*check_paths_access(char **paths, t_ast_node *node, t_mshell *obj)
-{
-	char	*path;
-
-	node->args = check_args(node->args);
-	check_is_dir(node->args[0], obj);
-	if (node->args[0][0] == '/' || node->args[0][0] == '.')
-		return (ft_strdup(node->args[0]));
-	path = check_paths(paths, node->args, &obj->args_move);
 	if (path && !*path)
 	{
 		check_free_str(&path);
 		exit_child(obj, NULL, 0, 0);
 	}
-	else if (!path || !*(node->args) + obj->args_move || !*(*(node->args) + obj->args_move))
+	else if (!path || !node_arg || !*node_arg)
 	{
 		check_free_str(&path);
-		if (!*(node->args) + obj->args_move || !*(*(node->args) + obj->args_move))
+		if (!node_arg || !*node_arg)
 		{
 			obj->exit_code = 0;
 			clean_mshell(obj);
@@ -93,6 +85,24 @@ char	*check_paths_access(char **paths, t_ast_node *node, t_mshell *obj)
 		obj->exit_code = 127;
 		print_exit("command not found\n", node->args[0 + obj->args_move], obj);
 	}
+	return (path);
+}
+
+char	*check_paths_access(char **paths, t_ast_node *node, t_mshell *obj)
+{
+	char		*path;
+	static char	*new_args[] = {"", NULL};
+
+	if (!node)
+		return (NULL);
+	if (!node->args)
+		node->args = new_args;
+	check_is_dir(node->args[0], obj);
+	if (node->args[0][0] == '/' || node->args[0][0] == '.')
+		return (ft_strdup(node->args[0]));
+	path = search_paths(paths, node->args, &obj->args_move);
+	if (!check_path(path, *(node->args) + obj->args_move, obj, node))
+		return (NULL);
 	check_is_dir(path, obj);
 	return (path);
 }
